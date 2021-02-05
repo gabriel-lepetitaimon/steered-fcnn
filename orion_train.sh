76ec2b2cad2d0de3d5747920abacff1a4bfef04d
Gabriel Lepetit-Aimon <gabriel.lepetitaimon@gmail.com>
2021-02-05 07:43:15 -0500

    ...

--- orion_train.sh
+++ orion_train.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 EXP_MAX_TRIAL=25
 
-EXP_NAME="roteq-6d3-truepdir"
-EXP_PATH="experiments/$EXP_NAME"
-orion -v --debug hunt -c "orion_config.yaml" -n "$EXP_NAME" --exp-max-trials $EXP_MAX_TRIAL python3 main_train.py --config "$EXP_PATH/exp_config.yaml"
-
-EXP_NAME="roteq-8-truepdir"
+EXP_NAME="roteq-4"
 EXP_PATH="experiments/$EXP_NAME"
 orion -v --debug hunt -c "orion_config.yaml" -n "$EXP_NAME" --exp-max-trials $EXP_MAX_TRIAL python3 main_train.py --config "$EXP_PATH/exp_config.yaml"
 
