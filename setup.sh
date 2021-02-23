a97f543891309012997ab1e2bbffecccfcd773e1
Gabriel LEPETIT-AIMON <gabriel.lepetit-aimon@polymtl.ca>
2021-02-23 13:10:19 -0500

    Update setup scripts

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install --no-index --upgrade pip
 
-pip install numpy torch pytorch-lightning mlflow albumentations opencv-python orion pyyaml pysftp
+pip install numpy torch torchvision pytorch-lightning mlflow albumentations opencv-python orion pyyaml pysftp h5py
--- setup_ComputeCanada.sh
+++ setup_ComputeCanada.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pip install --no-index --upgrade pip
-pip install numpy torch  mlflow  opencv_python orion PyYAML   --no-index
-pip install pytorch-lightning albumentations pysftp
+pip install numpy torch torchvision mlflow  opencv_python PyYAML scikit-learn orion h5py --no-index
+pip uninstall mlflow orion
+pip install pytorch-lightning albumentations pysftp orion mlflow
