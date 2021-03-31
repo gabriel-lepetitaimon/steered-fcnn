Fixing Rotation Data Augmentation

run_train.py
@@ -47,7 +47,6 @@ def run_train(opt=None):
         modelCheckpoints[metric] = checkpoint
         callbacks.append(checkpoint)
 
-    print('Stating training')
     trainer = pl.Trainer(gpus=args.gpus, callbacks=callbacks,
                          max_epochs=int(np.ceil(max_epoch/val_n_epoch)*val_n_epoch),
                          check_val_every_n_epoch=val_n_epoch,
@@ -187,6 +186,7 @@ def setup_log(cfg):
 
 
 def load_dataset(cfg):
+    from src.data_augment import DataAugment
     import albumentations as A
     from albumentations.pytorch import ToTensorV2
     from torch.utils.data import Dataset, DataLoader
@@ -203,21 +203,16 @@ def load_dataset(cfg):
 
             self.data = DATA.get(f'{dataset}/data')
             self.av = DATA.get(f'{dataset}/av')
-            self.field = DATA.get(f'{dataset}/radial-field')
+            self.alpha = DATA.get(f'{dataset}/principal-angle')
             self.mask = DATA.get(f'{dataset}/mask')
 
-            self.geo_aug = A.Compose([
-                #A.PadIfNeeded(800, 800, value=0, border_mode=cv2.BORDER_CONSTANT),
-                #A.RandomCrop(da_config['crop-size'], da_config['crop-size']),
-                A.HorizontalFlip(p=0.5),
-                A.Rotate(limit=(-180, 180)),
-                A.ElasticTransform(alpha=da_config['elastic-transform']['alpha'],
-                                   sigma=da_config['elastic-transform']['sigma'],
-                                   alpha_affine=da_config['elastic-transform']['alpha-affine'],
-                                   border_mode=cv2.BORDER_CONSTANT, p=.9),
-                A.VerticalFlip(p=0.5),
-                ToTensorV2()
-            ])
+            self.geo_aug = (DataAugment().flip()
+                                         .rotate()
+                                         .elastic_distortion(alpha=da_config['elastic-transform']['alpha'],
+                                                             sigma=da_config['elastic-transform']['sigma'],
+                                                             alpha_affine=da_config['elastic-transform']['alpha-affine'],
+                                                             border_mode=cv2.BORDER_CONSTANT)
+                           ).compile(images='x', labels='y,mask', angles='alpha', to_torch=True)
             self.factor = factor
             self._data_length = len(self.data)
 
@@ -226,22 +221,8 @@ def load_dataset(cfg):
 
         def __getitem__(self, i):
             i = i % self._data_length
-            img = [self.data[i].transpose(1, 2, 0)]
-            princ_dir_mode = cfg['model']['static-principal-direction']
-            if princ_dir_mode:
-                princ_dir = self.field[i]
-                if princ_dir_mode == 'normalized':
-                    princ_dir /= np.sqrt(princ_dir[0]**2+princ_dir[1]**2) + 1e-5
-                img += [princ_dir.transpose(1, 2, 0)]
-            img = np.concatenate(img, axis=2)
-            m = 1*(self.av[i] != 0)+self.mask[i]*16
-            d = self.geo_aug(image=img, mask=m)
-            r = {'x': d['image'][:6],
-                 'y': (d['mask'] % 16).int(),
-                 'mask': d['mask']//16}
-            if princ_dir_mode:
-                r['principal_direction'] = d['image'][6:]
-            return r
+            img = self.data[i].transpose(1, 2, 0)
+            return self.geo_aug(x=img, y=self.av[i], mask=self.mask[i], alpha=self.alpha[i])
 
     class TestDataset(Dataset):
         def __init__(self, dataset, file='DATA/vessels.h5'):
@@ -251,27 +232,17 @@ def load_dataset(cfg):
             
             self.data = DATA.get(f'{dataset}/data')
             self.av = DATA.get(f'{dataset}/av')
-            self.field = DATA.get(f'{dataset}/radial-field')
+            self.alpha = DATA.get(f'{dataset}/principal-angle')
             self.mask = DATA.get(f'{dataset}/mask')
-            self.geo_aug = A.Compose([#A.PadIfNeeded(800, 800, value=0, border_mode=cv2.BORDER_CONSTANT),
-                                      ToTensorV2()])
+            self.geo_aug = DataAugment().compile(images='x', labels='y,mask', angles='alpha', to_torch=True)
             self._data_length = len(self.data)
 
         def __len__(self):
             return self._data_length
 
         def __getitem__(self, i):
-            img = np.concatenate(
-                [self.data[i].transpose(1, 2, 0),
-                 self.field[i].transpose(1, 2, 0)],
-                axis=2)
-            m = 1*(self.av[i] != 0)+self.mask[i]*16
-            d = self.geo_aug(image=img, mask=m)
-            r = {'x': d['image'][:6],
-                 'principal_direction': d['image'][6:],
-                 'y': (d['mask'] % 16).int(),
-                 'mask': d['mask']//16}
-            return r
+            img = self.data[i].transpose(1, 2, 0)
+            return self.geo_aug(x=img, y=self.av[i], mask=self.mask[i], alpha=self.alpha[i])
 
     batch_size = cfg['hyper-parameters']['batch-size']
     train_dataset = cfg.training['training-dataset']

src/model.py
@@ -194,44 +194,20 @@ class HemelingRotNet(nn.Module):
         
         self.dropout = torch.nn.Dropout(p_dropout) if p_dropout else lambda x: x
 
-    def forward(self, x, principal_direction=None, **kwargs):
+    def forward(self, x, alpha=None, **kwargs):
         from functools import reduce
-        if not self.static_principal_direction or principal_direction is None:
+        if alpha is None:
             raise NotImplementedError()
-            
-            if self.principal_direction == 'all':
-                downSampledX = x.mean(axis=1)
-            else:
-                downSampledX = x[:, self.principal_direction]
-            device = self.final_conv.weight.device
-            alpha1 = compute_pdir(downSampledX, device=device,
-                                 hessian_value_threshold=self.principal_direction_hessian_threshold,
-                                 smooth_std=self.principal_direction_smooth)[:, None, :, :]
-            downSampledX = F.avg_pool2d(downSampledX, 2)
-            alpha2 = compute_pdir(downSampledX, device=device,
-                                 hessian_value_threshold=self.principal_direction_hessian_threshold,
-                                 smooth_std=self.principal_direction_smooth)[:, None, :, :]
-            downSampledX = F.avg_pool2d(downSampledX, 2)
-            alpha3 = compute_pdir(downSampledX, device=device,
-                                 hessian_value_threshold=self.principal_direction_hessian_threshold,
-                                 smooth_std=self.principal_direction_smooth)[:, None, :, :]
-            downSampledX = F.avg_pool2d(downSampledX, 2)
-            alpha4 = compute_pdir(downSampledX, device=device,
-                                 hessian_value_threshold=self.principal_direction_hessian_threshold,
-                                 smooth_std=self.principal_direction_smooth)[:, None, :, :]
-            downSampledX = F.avg_pool2d(downSampledX, 2)
-            alpha5 = compute_pdir(downSampledX, device=device,
-                                 hessian_value_threshold=self.principal_direction_hessian_threshold,
-                                 smooth_std=self.principal_direction_smooth)[:, None, :, :]
         else:
             with torch.no_grad():
-                alpha = torch.atan2(principal_direction[:, 1], principal_direction[:, 0])
+                if alpha.dim == 4 and alpha.shape[1] == 2:
+                    alpha = torch.atan2(alpha[:, 1], alpha[:, 0])
                 
                 max_k = self.base.max_k
                 b, h, w = alpha.shape
                 
-                k_alpha = torch.stack([k*alpha for k in range(1,self.base.max_k+1)])
-                cos_sin_kalpha = torch.stack((torch.cos(k_alpha),torch.sin(k_alpha)))
+                k_alpha = torch.stack([k*alpha for k in range(1, self.base.max_k+1)])
+                cos_sin_kalpha = torch.stack((torch.cos(k_alpha), torch.sin(k_alpha)))
                 
                 alpha1 = cos_sin_kalpha.reshape((2*max_k,b,h,w))
                 alpha2 = F.avg_pool2d(alpha1, 2)

src/steered_cnn.py
@@ -91,7 +91,7 @@ class SteerableKernelBase(KernelBase):
 
         nn.init.normal_(w[..., self.idx_equi()], std=std_equi)
 
-        std_ortho = gain*np.sqrt(3 / (n_in * self.K_steer * 2))
+        std_ortho = gain*np.sqrt(3 / (n_in * self.K_steer))
         nn.init.normal_(w[..., self.idx_real()], std=std_ortho)
         nn.init.normal_(w[..., self.idx_imag()], std=std_ortho)
 
@@ -266,6 +266,54 @@ class SteerableKernelBase(KernelBase):
         psi_real, psi_imag = self.base[real_idx], self.base[imag_idx]
 
         return KernelBase.composite_kernels(w_real, psi_imag) - KernelBase.composite_kernels(w_imag, psi_real)
+    
+    def format_weights(self, weights, mean=True):
+        from pandas import DataFrame
+        from .utils import iter_index
+        data = dict(r=[_['r'] for _ in self.kernels_info],
+                    k=[_['k'] for _ in self.kernels_info],
+                    type=[_['type'] for _ in self.kernels_info])
+        if isinstance(weights, torch.Tensor):
+            weights = weights.detach().cpu().numpy()
+        s = weights.shape[:-1]
+        if not len(s) or np.prod(s)==1:
+            data['weight'] = weights.flatten()
+        else:
+            for idx in iter_index(weights.shape):
+                    data[f'weights{list(idx)}'] = weights[idx]
+            if mean:
+                data['weights_mean'] = weights.mean(axis=tuple(range(len(s))))
+                data['weights_std'] = weights.std(axis=tuple(range(len(s))))
+        return DataFrame(data=data)
+    
+    def weights_dist(self, weights, Q=3):
+        from pandas import DataFrame
+        if isinstance(weights, torch.Tensor):
+            weights = weights.detach().cpu().numpy()
+        weights = weights.reshape((np.prod(weights.shape[:-1]), weights.shape[-1]))
+        data = dict(r=[_['r'] for _ in self.kernels_info],
+                    k=[_['k'] for _ in self.kernels_info],
+                    type=[_['type'] for _ in self.kernels_info],
+                    name=[f'r={_["r"]}, k={_["k"]}, {"Real" if _["type"]=="R" else "Imag"}' for _ in self.kernels_info])
+        if isinstance(Q, int):
+            Q = [i/(Q+1) for i in range(Q)]
+        q = np.array(Q)*100
+        q = q/2
+        q = np.concatenate([50-q[::-1], [50], 50+q]).flatten()
+        perc = np.percentile(weights, q, axis=0)
+        data['median'] = perc[len(Q)]
+        
+        for i, q in enumerate(Q):
+            data[f'q{i}']= perc[-i-1]
+            data[f'-q{i}']= perc[i]
+        return data
+        
+        
+    
+    def plot_weights(self, weights):
+        pass
+        import altair as alt
+        
 
 
 _DEFAULT_STEERABLE_BASE = SteerableKernelBase.create_from_rk(4, max_k=5)