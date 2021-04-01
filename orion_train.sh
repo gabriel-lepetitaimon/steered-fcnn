66cef1f52825b6d7b83c88db546a6cafa1b21dde
Gabriel Lepetit-Aimon <gabriel.lepetitaimon@gmail.com>
2021-03-31 22:10:32 -0400

    Refactoring

--- orion_train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-EXP_MAX_TRIAL=25
-
-EXP_NAME="roteq-4"
-EXP_PATH="experiments/$EXP_NAME"
-orion -v --debug hunt -c "orion_config.yaml" -n "$EXP_NAME" --exp-max-trials $EXP_MAX_TRIAL python3 main_train.py --config "$EXP_PATH/exp_config.yaml"
-
