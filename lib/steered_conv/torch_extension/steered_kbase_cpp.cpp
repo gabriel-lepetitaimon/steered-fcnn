6add039c1491945860d3ba1b9aac33156453d5fb
Gabriel Lepetit-Aimon <gabriel.lepetit-aimon@polymtl.ca>
2021-04-06 18:25:39 -0400

    Working on cpp implementation.

--- lib/steered_conv/torch_extension/steered_kbase_cpp.cpp
+++ lib/steered_conv/torch_extension/steered_kbase_cpp.cpp
@@ -16,6 +16,15 @@ std::vector<at::Tensor> conv2d_cosSinAlpha_forward(
     torch::
 }
 
+std::vector<at::Tensor> conv2d_cosSinKAlpha_forward(
+        torch::Tensor input,    // shape: (b, n_in, h, w)
+        torch::Tensor weights,  // shape: (n_out, n_in, K)
+        torch::Tensor real_kernels,
+        torch::Tensor imag_kernels,  // shape: (n_out, n_in, K)
+        torch::Tensor cos_sin_kalpha) {
+    torch::
+}
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
   m.def("forward", &conv2d_forward, "steerable_kbase conv2d forward");
 }
