cf34b3b16a96b38d71fd317f9e3e58ddbf4dffcd
Gabriel Lepetit-Aimon <gabriel.lepetit-aimon@polymtl.ca>
2021-04-09 19:04:07 -0400

    Notebooks update.

--- notebooks/ImplementationCheck/sources.py
+++ notebooks/ImplementationCheck/sources.py
@@ -3,5 +3,7 @@ import os.path as P
 sys.path.insert(0, P.abspath(P.join(P.dirname(__file__), '../../lib/')))
 sys.path.insert(0, P.abspath(P.join(P.dirname(__file__), '../../experimentations/')))
 
-from src.config import default_config
+from src.config import default_config, parse_config
 from src.datasets import load_dataset
+from run_train import setup_model
+import steered_cnn
