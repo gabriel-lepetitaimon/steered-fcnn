3d4dde315c1c513b382b1d490e6796ef08986609
Gabriel Lepetit-Aimon <gabriel.lepetit-aimon@polymtl.ca>
2021-04-20 10:22:54 -0400

    Add first cpp implementations files

--- lib/steered_cnn/steered_conv/torch_extension/steered_kbase_common.h
+++ lib/steered_cnn/steered_conv/torch_extension/steered_kbase_common.h
@@ -1,6 +1,17 @@
 #include <torch/extension.h>
 
-using namespace torch::indexing;
+torch::TensorList cos_sin_ka(const torch::Tensor &cos_a, const torch::Tensor &sin_a, 
+                          const torch::Tensor &cos_km1_a, const torch::Tensor &sin_km1_a) {
+    // cos(ka) = cos(a) cos( (k-1)a ) - sin(a) sin( (k-1)a )
+    auto cos_ka = cos_a*cos_km1_a;
+    cos_ka.addcmul_(sin_a, sin_km1_a, -1);
+
+    // sin(ka) = cos(a) sin( (k-1)a ) + sin(a) cos( (k-1)a )
+    auto sin_ka = cos_a*sin_km1_a;
+    sin_ka.addcmul_(sin_a, cos_km1_a);
+
+    return {cos_ka, sin_ka};
+}
 
 /**
     Computes the matrix:
@@ -8,18 +19,19 @@ using namespace torch::indexing;
          [sin(α), sin(2α), ..., sin(kα)]]
 
         Args:
-            cos_sin_a: The tensor [cos(α), sin(α)] of shape [2, n0, n1, ...]
+            cos_sin_a: The tensor [cos(α), sin(α)] of shape [2, b, n_out, h, w]
 
             k: The max k
 
-        Returns: The tensor cos_sin_ka of shape [2, k, n0, n1, ...], where:
+        Returns: The tensor cos_sin_ka of shape [2, k, b, n_out, h, w], where:
                     cos_sin_ka[0] = [cos(α), cos(2α), ..., cos(kα)]
                     cos_sin_ka[1] = [sin(α), sin(2α), ..., sin(kα)]
 */
-torch::Tensor cos_sin_ka_stack(torch::Tensor cos_sin_a, int k){
-    const auto cos_a = cos_sin_a[0];
-    const auto sin_a = cos_sin_a[1];
-    TensorList
-    torch::addcmul()
+torch::ArrayRef<torch::TensorList> cos_sin_ka_stack(const torch::Tensor &cos_a, const torch::Tensor &sin_a, int k){
+    torch::TensorList out[k];
+    out[0] = torch::TensorList({cos_a, sin_a});
+    for(int i=1; i<k; i++) {
+        out[i] = cos_sin_ka(cos_a, sin_a, out[i-1][0], out[i-1][1]);
+    }
+    return torch::ArrayRef<torch::TensorList>(out, k);
 }
-
--- lib/steered_cnn/steered_conv/torch_extension/steered_kbase_cpp.cpp
+++ lib/steered_cnn/steered_conv/torch_extension/steered_kbase_cpp.cpp
@@ -1,12 +1,11 @@
 #include <torch/extension.h>
 
-#include <iostream>
-
 torch::Tensor d_sigmoid(torch::Tensor z) {
   auto s = torch::sigmoid(z);
   return (1 - s) * s;
 }
 
+/*
 std::vector<at::Tensor> conv2d_cosSinAlpha_forward(
         torch::Tensor input,    // shape: (b, n_in, h, w)
         torch::Tensor weights,  // shape: (n_out, n_in, K)
@@ -22,9 +21,47 @@ std::vector<at::Tensor> conv2d_cosSinKAlpha_forward(
         torch::Tensor real_kernels,
         torch::Tensor imag_kernels,  // shape: (n_out, n_in, K)
         torch::Tensor cos_sin_kalpha) {
-    torch::
+    
+}
+*/
+
+torch::Tensor cos_sin_ka(const torch::Tensor &cos_a, const torch::Tensor &sin_a, 
+                          const torch::Tensor &cos_km1_a, const torch::Tensor &sin_km1_a) {
+    // cos(ka) = cos(a) cos( (k-1)a ) - sin(a) sin( (k-1)a )
+    auto cos_ka = cos_a*cos_km1_a;
+    cos_ka.addcmul_(sin_a, sin_km1_a, -1);
+
+    // sin(ka) = cos(a) sin( (k-1)a ) + sin(a) cos( (k-1)a )
+    auto sin_ka = cos_a*sin_km1_a;
+    sin_ka.addcmul_(sin_a, cos_km1_a);
+    return torch::stack({cos_ka, sin_ka});
 }
 
+/**
+    Computes the matrix:
+        [[cos(α), cos(2α), ..., cos(kα)],
+         [sin(α), sin(2α), ..., sin(kα)]]
+
+        Args:
+            cos_sin_a: The tensor [cos(α), sin(α)] of shape [2, b, n_out, h, w]
+
+            k: The max k
+
+        Returns: The tensor cos_sin_ka of shape [2, k, b, n_out, h, w], where:
+                    cos_sin_ka[0] = [cos(α), cos(2α), ..., cos(kα)]
+                    cos_sin_ka[1] = [sin(α), sin(2α), ..., sin(kα)]
+
+torch::ArrayRef<torch::TensorList> cos_sin_ka_stack(const torch::Tensor &cos_a, const torch::Tensor &sin_a, int k){
+    torch::TensorList out[k];
+    out[0] = torch::TensorList({cos_a, sin_a});
+    for(int i=1; i<k; i++) {
+        out[i] = cos_sin_ka(cos_a, sin_a, out[i-1][0], out[i-1][1]);
+    }
+    return torch::ArrayRef<torch::TensorList>(out, k);
+}
+*/
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-  m.def("forward", &conv2d_forward, "steerable_kbase conv2d forward");
+  //m.def("forward", &conv2d_forward, "steerable_kbase conv2d forward");
+  //m.def("cos_sin_ka_stack", &cos_sin_ka_stack, "cos_sin_ka_stack");
+  m.def("cos_sin_ka", &cos_sin_ka, "cos_sin_ka");
 }
