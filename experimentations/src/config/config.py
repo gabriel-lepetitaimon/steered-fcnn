857176fc62adf61ad183e8b70a1436471a138a59
Gabriel LEPETIT-AIMON <gabriel.lepetit-aimon@polymtl.ca>
2021-11-10 15:24:49 -0500

    Bug fix.

--- experimentations/src/config/config.py
+++ experimentations/src/config/config.py
@@ -30,7 +30,7 @@ def parse_config(cfg_file):
 
     # --- Preprocess cfg file
     if "sub-experiment" not in exp_config['experiment']:
-        exp_config['experiment'] = f'[{exp_config["exp"]}] {exp_config["sub"]}'
+        exp_config['experiment'] = f'[{exp_config.tags["exp"]}] {exp_config.tags.get("sub","")}'
     exp_config = exp_config.filter(lambda k, v: not (isinstance(v, str) and v.startswith('orion~')), recursive=True)
 
     return default_cfg.recursive_update(exp_config)
