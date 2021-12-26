Fix kernel base normalization.

lib/steered_cnn/steered_conv/ortho_kbase.py
@@ -9,7 +9,7 @@ from .steerable_filters import radial_steerable_filter
 
 
 class OrthoKernelBase(KernelBase):
-    def __init__(self, base: Union[np.ndarray, torch.Tensor], autonormalize=True):
+    def __init__(self, base: Union[np.ndarray, torch.Tensor]):
         """
 
         Args:
@@ -22,7 +22,7 @@ class OrthoKernelBase(KernelBase):
             base = torch.from_numpy(base).to(dtype=torch.float)
         base = torch.cat((base, torch.rot90(base, 1, (-2, -1))), dim=0)
 
-        super(OrthoKernelBase, self).__init__(base, autonormalize=autonormalize)
+        super(OrthoKernelBase, self).__init__(base)
         self.K = base.shape[0] // 2
         self.kernels_label = None
         self.kernels_info = None
@@ -93,9 +93,8 @@ class OrthoKernelBase(KernelBase):
         raise ValueError(f'Invalid OrthoKernelBase specs: {spec}.')
 
     @staticmethod
-    def create_radial(kernel_size: int, std=.5, size=-1, phase=None, autonormalize=True, oversample=100):
+    def create_radial(kernel_size: int, std=.5, size=-1, phase=None, oversample=100):
         """
-
 
         Args:
             kr: A specification of which steerable filters should be included in the base.
@@ -142,9 +141,9 @@ class OrthoKernelBase(KernelBase):
             size = int(np.ceil(2*(r_max+std)))
 
         kernels = [np.real(radial_steerable_filter(size=size, k=1, r=r, phase=phase,
-                                                   std=std, oversampling=oversample))
+                                                   std=std, oversampling=oversample, normalize=True))
                    for r in R]
-        base = OrthoKernelBase(np.stack(kernels), autonormalize=autonormalize)
+        base = OrthoKernelBase(np.stack(kernels))
         base.kernels_label = [f'r{r}R' for r in R] + [f'r{r}I' for r in R]
         base.kernels_info = [{'r': r, 'type': 'R'} for r in R] + [{'r': r, 'type': 'I'} for r in R]
         return base

lib/steered_cnn/steered_conv/steerable_filters.py
@@ -14,7 +14,7 @@ def max_steerable_harmonics(radius):
     return int(inter_area//2)-1
 
 
-def radial_steerable_filter(size, k, r, std=.5, oversampling=1, phase=0):
+def radial_steerable_filter(size, k, r, std=.5, oversampling=1, phase=0, normalize=False):
     from ..utils.rotequivariance_toolbox import polar_space
     oversampling = int(np.round(oversampling if oversampling >= 1 else 1))
     oversampled_size = size * oversampling
@@ -30,6 +30,12 @@ def radial_steerable_filter(size, k, r, std=.5, oversampling=1, phase=0):
     f = G*PHI
     if oversampling > 1:
         f = f.reshape((size, oversampling, size, oversampling)).mean(axis=(1, 3))
+    if normalize is True or normalize == 'square':
+        f /= np.sqrt((f**2).sum())+1e-8
+    elif normalize == 'abs':
+        f /= np.abs(f).sum()+1e-8
+    elif normalize == 'sum':
+        f /= np.abs(f.sum())+1e-8
     return f
 
 

lib/steered_cnn/steered_conv/steered_kbase.py
@@ -12,14 +12,14 @@ from typing import Union, Dict, List
 
 
 class SteerableKernelBase(KernelBase):
-    def __init__(self, base: 'list(np.array) [K,n,m]', n_kernel_by_k: 'dict {k -> n_k}', autonormalize=True):
+    def __init__(self, base: 'list(np.array) [K,n,m]', n_kernel_by_k: 'dict {k -> n_k}'):
         """
 
         Args:
             base: kernels are assumed to be sorted by ascending k
             n_kernel_by_k:
         """
-        super(SteerableKernelBase, self).__init__(base, autonormalize=autonormalize)
+        super(SteerableKernelBase, self).__init__(base, autonormalize=False)
 
         # Sorting n_filter_by_k and removing invalid values
         self.n_filter_by_k = OrderedDict()
@@ -171,7 +171,7 @@ class SteerableKernelBase(KernelBase):
 
     @staticmethod
     def create_radial(kr: Union[int, Dict[int, List[int]]], std=.5, size=-1, oversample=16,
-                      phase=None, max_k=None, autonormalize=True):
+                      phase=None, max_k=None):
         """
 
 
@@ -187,7 +187,6 @@ class SteerableKernelBase(KernelBase):
             size:
             oversample:
             max_k:
-            autonormalize:
 
         Returns: A SteerableKernelBase parametrized by the corresponding kernels.
 
@@ -245,6 +244,7 @@ class SteerableKernelBase(KernelBase):
                     n_kernel_by_k[k] = 1
 
                 psi = radial_steerable_filter(size, k, r, std=std, oversampling=oversample, phase=phase)
+                psi /= np.sqrt((psi**2).sum())
 
                 labels_real += [f'k{k}r{r:.4g}'+('R' if k > 0 else '')]
                 info_real += [{'k': k, 'r': r, 'type': 'R'}]
@@ -255,9 +255,8 @@ class SteerableKernelBase(KernelBase):
                     kernels_imag += [psi.imag]
 
         K = np.stack(kernels_real + kernels_imag)
-        # K /= np.sqrt((K**2).sum((1, 2)).mean())
 
-        B = SteerableKernelBase(K, n_kernel_by_k=n_kernel_by_k, autonormalize=autonormalize)
+        B = SteerableKernelBase(K, n_kernel_by_k=n_kernel_by_k)
         B.kernels_label = labels_real + labels_imag
         B.kernels_info = info_real + info_imag
         return B